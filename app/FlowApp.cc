2b5ab73808ab906e2c74b97169bfb751d1b4c86d
jhsheng <jhsheng@mail.ustc.edu.cn>
2023-03-19 23:28:01 +0800

    TODO 增加有流大小分布的App

---
+++ app/FlowApp.cc
@@ -0,0 +1,13 @@
+#include "FlowApp.h"
+
+Define_Module(FlowApp);
+
+void FlowApp::initialize()
+{
+    // TODO - Generated method body
+}
+
+void FlowApp::handleMessage(cMessage *msg)
+{
+    // TODO - Generated method body
+}
--- app/FlowApp.h
+++ app/FlowApp.h
@@ -1,18 +1,12 @@
-#ifndef __ROUTING_BUFFER_H_
-#define __ROUTING_BUFFER_H_
-
+#pragma once
 #include <omnetpp.h>
 
 using namespace omnetpp;
 
-/**
- * TODO - Generated class
- */
-class Buffer : public cSimpleModule
+class FlowApp : public cSimpleModule
 {
   protected:
     virtual void initialize() override;
     virtual void handleMessage(cMessage *msg) override;
 };
 
-#endif
