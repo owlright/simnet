b90f0eb3268a429949a6e5f1123af8406ae1a05b
jhsheng <jhsheng@mail.ustc.edu.cn>
2023-03-22 20:33:48 +0800

    TODO: FlowApp能读取load参数生成interval

--- app/FlowApp.cc
+++ app/FlowApp.cc
@@ -1,13 +1,37 @@
 #include "FlowApp.h"
 
 Define_Module(FlowApp);
-
-void FlowApp::initialize()
+FlowApp::~FlowApp()
 {
-    // TODO - Generated method body
+    cancelAndDelete(selfMsg);
+}
+
+void FlowApp::initialize(int stage)
+{
+    if (stage == Stage::INITSTAGE_LOCAL) {
+        socket = (Socket*)(getSubmodule("socket"));
+        selfMsg = new cMessage("SelfMsg-FlowApp");
+        double load = par("load");
+        if (load != 0) { // load mode
+            assert(0 < load && load < 1);
+            auto flowLengthMean = par("flowLengthMean").doubleValueInUnit("b");
+            // !hacky
+            auto bandwidth = check_and_cast<cDatarateChannel *>(getParentModule()->gateHalf("port", cGate::Type::OUTPUT, 0)->getChannel())->getDatarate();
+            loadModeEnabled = true;
+            interval = flowLengthMean / (bandwidth * load);
+            EV << "load: " << load << " flowLengthMean: " << flowLengthMean <<" bandwidth: " << bandwidth << endl;
+        }
+        if (loadModeEnabled) {
+            scheduleAfter(exponential(interval), selfMsg);
+        } else {
+            scheduleAfter(par("arrivalInterval"), selfMsg);
+        }
+    }
 }
 
 void FlowApp::handleMessage(cMessage *msg)
 {
-    // TODO - Generated method body
+    if (msg->isSelfMessage()) {
+
+    }
 }
--- app/FlowApp.h
+++ app/FlowApp.h
@@ -1,12 +1,24 @@
 #pragma once
 #include <omnetpp.h>
-
+#include "../mod/Packet_m.h"
+#include "../mod/socket.h"
+#include "../common/Defs.h"
 using namespace omnetpp;
 
 class FlowApp : public cSimpleModule
 {
-  protected:
-    virtual void initialize() override;
+public:
+    ~FlowApp();
+protected:
+    bool loadModeEnabled{false};
+    simtime_t interval{SIMTIME_ZERO};
+private:
+    Socket* socket = nullptr;
+    // state
+    cMessage *selfMsg{nullptr};
+protected:
+    virtual void initialize(int stage) override;
     virtual void handleMessage(cMessage *msg) override;
+    virtual int numInitStages() const override { return NUM_INIT_STAGES; }
 };
 
