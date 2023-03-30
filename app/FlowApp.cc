10fb3668e345f7b1149c4dcf8a0efb5b22832c4b
jhsheng <jhsheng@mail.ustc.edu.cn>
2023-03-30 20:11:20 +0800

    change: 如果是汇聚组里的发送端，读取另外的数据长度参数和interval

--- app/FlowApp.cc
+++ app/FlowApp.cc
@@ -18,53 +18,61 @@ void FlowApp::initialize(int stage)
         socket = (Socket*)(getSubmodule("socket"));
         flowRealCompletionTimeSignal = registerSignal("flowRealCompletionTime");
         flowIdealCompletionTimeSignal = registerSignal("flowIdealCompletionTime");
+        bandwidth = check_and_cast<cDatarateChannel *>(getParentModule()->gateHalf("port", cGate::Type::OUTPUT, 0)->getChannel())->getDatarate();
     }
-    if (stage == Stage::INITSTAGE_LOCAL) {
-        startFlowTimer = new cMessage("flowStart");
-        double load = par("load");
-        if (load != 0) { // load mode
-            assert(0 < load && load < 1);
-            auto flowLengthMean = par("flowLengthMean").doubleValueInUnit("b");
-            // !HACK
-            bandwidth = check_and_cast<cDatarateChannel *>(getParentModule()->gateHalf("port", cGate::Type::OUTPUT, 0)->getChannel())->getDatarate();
-            loadModeEnabled = true;
-            interval = flowLengthMean / (bandwidth * load);
-            EV_DETAIL << "load: " << load << " flowLengthMean: " << flowLengthMean <<" bandwidth: " << bandwidth << endl;
-        }
 
-        if (loadModeEnabled) {
-            scheduleAfter(exponential(interval), startFlowTimer);
-        } else {
-            scheduleAfter(par("arrivalInterval"), startFlowTimer);
-        }
-    }
     if (stage == Stage::INITSTAGE_CONTROLL) {
+        double load = par("load");
         // HACK
         auto controller = getModuleFromPar<Controller>(this->getParentModule()->par("globalController"), this->getParentModule());
         if (destAddress == -2) { // means random pattern, ask for controller
             destAddress = controller->askForDest(myAddress);
             EV_DETAIL << "src:" << myAddress << " dest:" << destAddress << endl;
-            socket->Bind(myAddress, destAddress, groupAddress); // ! TODO the socket is unicast at first, it may change into a aggrsocket
+//            socket->Bind(myAddress, destAddress, groupAddress); // ! TODO the socket is unicast at first, it may change into a aggrsocket
         }
         if (groupAddress == -2) { // ask for controller to see if myself in a aggr group,-1 means not particapte in aggr group
             groupAddress = controller->askForGroup(myAddress);
+            load = 0;
             if (groupAddress == myAddress) { // I'm the root node of the aggregation tree
                 auto groupSenders = controller->getAggrSendersNum(myAddress);
-                EV << "node "<< myAddress << " is group target and has " << groupSenders << " senders" << endl;
-                socket->Bind(myAddress, destAddress, groupAddress);
                 socket->SetSendersNum(groupSenders);
+                EV << "node "<< myAddress << " is group target and has " << groupSenders << " senders" << endl;
+            } else if (groupAddress > 0) { // I'm the sender
+                destAddress = groupAddress;
+                EV_DETAIL << "src:" << myAddress << " group:" << groupAddress << endl;
+                flowLength = &par("aggFlowLength");
             }
-            destAddress = groupAddress; // groupAddress is also the destAddress
-            if (groupAddress > 0)
-                EV_DETAIL << "src:" << myAddress << " dest:" << destAddress << " group:" << groupAddress << endl;
+
         }
+        socket->Bind(myAddress, destAddress, groupAddress);
+
+
+        startFlowTimer = new cMessage("flowStart");
+
+        if (load != 0) { // load mode
+            assert(0 < load && load < 1);
+            auto flowLengthMean = par("flowLengthMean").doubleValueInUnit("b");
+            // !HACK
+
+            loadModeEnabled = true;
+            interval = flowLengthMean / (bandwidth * load);
+            EV_DETAIL << "load: " << load << " flowLengthMean: " << flowLengthMean <<" bandwidth: " << bandwidth << endl;
+        }
+        if (myAddress != groupAddress) { // root node dont send packets
+            if (loadModeEnabled) {
+                scheduleAfter(exponential(interval), startFlowTimer);
+            } else {
+                scheduleAfter(par("arrivalInterval"), startFlowTimer);
+            }
+        }
+
     }
 }
 
 void FlowApp::handleMessage(cMessage *msg)
 {
     if (msg == startFlowTimer) {
-        if (destAddress != -1) {
+        if (destAddress != -1 || groupAddress != -1 ) {
             auto flowTotalBytes = flowLength->intValue();
             do {
                 flowTotalBytes = flowLength->intValue();
