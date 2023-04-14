3fed4e9ae6dd345b2100a8233a97264da22b222e
jhsheng <jhsheng@mail.ustc.edu.cn>
2023-04-14 16:02:58 +0800

    make the cmd work again

--- mingwenv.cmd
+++ mingwenv.cmd
@@ -1,2 +1,4 @@
-set HOME=%~d0\omnetpp-6.0.1
-call "%HOME%\tools\win32.x86_64\msys2_shell.cmd" -mingw64 -defterm -here -no-start %*
+set "CURRENT_DIR=%CD%"
+for %%I in ("%CURRENT_DIR%") do set "PARENT_DIR=%%~dpI"
+
+call %PARENT_DIR%omnetpp-6.0.1\tools\win32.x86_64\msys2_shell.cmd -mingw64 -defterm -here -no-start %*
