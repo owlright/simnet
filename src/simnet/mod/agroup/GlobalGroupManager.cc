9a90de1781e0fb94efe83f60a3e677077d355b3f
jhsheng <jhsheng@mail.ustc.edu.cn>
2023-04-27 21:10:53 +0800

    feat(GlobalGroupManager): read settings from txt file

--- src/simnet/mod/agroup/GlobalGroupManager.cc
+++ src/simnet/mod/agroup/GlobalGroupManager.cc
@@ -1,9 +1,37 @@
 #include "GlobalGroupManager.h"
+#include <fstream>
 
 Define_Module(GlobalGroupManager);
 
 AggGroupEntry *GlobalGroupManager::getGroupEntry(IntAddress group)
 {
-    auto entry = new AggGroupEntry();
+    auto entry = new AggGroupEntry(1, 1); // FIXME
     return entry;
+}
+
+void GlobalGroupManager::initialize(int stage)
+{
+    if (stage == INITSTAGE_LOCAL) {
+        std::fstream groupConfigFile(par("groupConfigFile").stringValue(), std::ios::in);
+        if (!groupConfigFile)
+            throw cRuntimeError("%s not found!", par("groupConfigFile").stringValue());
+        std::string line;
+        while (getline(groupConfigFile, line, '\n')) {
+            if (line.empty() || line[0] == '#')
+                continue;
+            std::vector<std::string> tokens = cStringTokenizer(line.c_str()).asVector();
+            if (tokens.size() != 4)
+                throw cRuntimeError("wrong line in module file: 3 items required, line: \"%s\"", line.c_str());
+            // get fields from tokens
+            long groupAddr = atol(tokens[0].c_str());
+            long treeIndex = atol(tokens[1].c_str());
+            long switchAddr = atol(tokens[2].c_str());
+            long fanIndegree = atol(tokens[3].c_str());
+            EV << "group_address=" << groupAddr
+               << " tree_index=" << treeIndex
+               << " switch_address=" << switchAddr
+               << " fanIndegree=" << fanIndegree
+               << endl;
+        }
+    }
 }
--- src/simnet/mod/agroup/GlobalGroupManager.h
+++ src/simnet/mod/agroup/GlobalGroupManager.h
@@ -11,7 +11,7 @@ public:
     AggGroupEntry* getGroupEntry(IntAddress group);
 
 protected:
-    virtual void initialize(int stage) override {};
+    virtual void initialize(int stage) override;
     virtual void handleMessage(cMessage *msg) override
         {throw cRuntimeError("this module doesn't handle messages, it runs only in initialize()");};
     virtual int numInitStages() const override { return NUM_INIT_STAGES; }
