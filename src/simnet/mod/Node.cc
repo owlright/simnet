d5f0384ca52c8d9f7898780a0cbe7960f058c6ba
jhsheng <jhsheng@mail.ustc.edu.cn>
2023-06-29 18:04:43 +0800

    refactor(Node): 将switch和host两种节点彻底分离

--- src/simnet/mod/Node.cc
+++ src/simnet/mod/Node.cc
@@ -1,41 +1,25 @@
 #include "Node.h"
 
+
 Define_Module(Node);
 
 void Node::initialize(int stage)
 {
     if (stage == INITSTAGE_LOCAL) {
         address = par("address").intValue();
-        isHost = getProperties()->get("host") != nullptr;
     }
-    else if (stage == INITSTAGE_LAST) {
-        if (isHost) {
-            auto apps = getSubmoduleArray("apps");
-            std::vector<const char*> parNames{"port", "destAddress", "destPort"};
-            for (auto app:apps) {
-                for (auto parname: parNames) {
-                    if (app->hasPar(parname)) {
-                        EV_TRACE << app->par(parname) << " ";
-                    }
-                }
-                EV_TRACE << endl;
-                if (app->hasPar("destAddress") &&
-                        app->par("destAddress").intValue() == INVALID_ADDRESS) {
-                    EV_WARN << "node " << address << " has idle sender " << app->getClassAndFullName() << endl;
-                    app->callFinish();
-                    app->deleteModule();
-                }
-            }
 
-        }
-
-    }
+}
+void Node::handleMessage(cMessage * msg)
+{
+    throw cRuntimeError("This is a base class, you shouldn't use it directly.");
 }
 
 void Node::handleParameterChange(const char *parameterName)
 {
     if (strcmp(parameterName, "address") == 0) {
-        address = par("address").intValue();
+        address = par("address");
+        // std::cout << getClassAndFullPath() << std::endl;
     }
 }
 
@@ -47,3 +31,75 @@ void Node::refreshDisplay() const
         getDisplayString().setTagArg("t", 0, buf);
     }
 }
+
+
+Define_Module(HostNode);
+
+void HostNode::initialize(int stage)
+{
+    Node::initialize(stage);
+    if (stage == INITSTAGE_LOCAL) {
+        load = par("load");
+
+        flowInterval = par("flowInterval");
+        bandwidth = par("bandwidth");
+        if (bandwidth <= 0) { // get bandwidth value automatically
+            bandwidth = check_and_cast<cDatarateChannel *>(
+                                        gateHalf("port", cGate::Type::OUTPUT, 0)
+                                        ->getChannel())->getDatarate();
+            par("bandwidth") = bandwidth; // ! apps will read this value
+        }
+        if (load > 0.0) {
+            flowSizeMean = par("flowSizeMean");
+            ASSERT(flowSizeMean > 0);
+            flowInterval = flowSizeMean / (bandwidth * load); // load cannot be zero
+        }
+        trafficPattern = par("trafficPattern");
+        newFlowTimer = new cMessage("newFlow");
+    }
+    else if (stage == INITSTAGE_ASSIGN) {
+
+    }
+}
+
+void HostNode::handleMessage(cMessage* msg)
+{
+    if (msg == newFlowTimer) {
+        startNewFlow();
+    }
+}
+
+
+opp_component_ptr<UnicastSenderApp>
+HostNode::createUnicastSenderApp()
+{
+    return opp_component_ptr<UnicastSenderApp>();
+}
+
+void HostNode::startNewFlow()
+{
+    auto apps = getSubmoduleArray("apps");
+    bool foundIdleApp = false;
+    for (auto mod:apps) {
+        if (strcmp(mod->getClassName(), "UnicastSenderApp") == 0) {
+            auto app = check_and_cast<UnicastSenderApp*>(mod);
+            if (app->getAppState() == Idle || app->getAppState() == Finished) {
+                app->par("flowStartTime") = (simTime() + exponential(flowInterval)).dbl();
+                foundIdleApp = true;
+            }
+        }
+        if (foundIdleApp == true)
+            break;
+    }
+    if (!foundIdleApp) { // we need to create a new app
+        auto app = createUnicastSenderApp();
+        app->par("flowStartTime") = (simTime() + exponential(flowInterval)).dbl();
+    }
+}
+
+HostNode::~HostNode()
+{
+    cancelAndDelete(newFlowTimer);
+}
+
+Define_Module(SwitchNode);
--- src/simnet/mod/Node.h
+++ src/simnet/mod/Node.h
@@ -1,20 +1,47 @@
 #include <omnetpp.h>
 #include "simnet/common/Defs.h"
-#include "simnet/app/UnicastApp.h"
-using namespace std;
+#include "simnet/app/UnicastSenderApp.h"
+using namespace omnetpp;
 
 class Node : public cSimpleModule
 {
+public:
+    virtual ~Node() {};
+
 protected:
     IntAddress address;
 
 protected:
     virtual void initialize(int stage) override;
-    virtual void handleMessage(cMessage * msg) override {};
+    virtual void handleMessage(cMessage * msg) override;
     virtual void handleParameterChange(const char *parameterName) override;
     virtual void refreshDisplay() const override;
     virtual int numInitStages() const override { return Stage::NUM_INIT_STAGES; }
 
+};
+
+class HostNode : public Node {
+public:
+    virtual ~HostNode();
+
+protected:
+    virtual void initialize(int stage) override;
+    virtual void handleMessage(cMessage * msg) override;
+
 private:
-    bool isHost;
+    opp_component_ptr<UnicastSenderApp> createUnicastSenderApp();
+    void startNewFlow();
+
+protected:
+    double load{0};
+    B flowSizeMean{0};
+    double flowInterval{0.0};
+    double bandwidth{0.0}; // bps
+    const char* trafficPattern;
+
+    cMessage* newFlowTimer{nullptr};
+};
+
+class SwitchNode : public Node {
+
 };
