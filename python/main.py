7e7ee55541678d8d7be3de43431192abad3bbb93
jhsheng <jhsheng@mail.ustc.edu.cn>
2023-08-01 23:02:59 +0800

    sync : 正则表达式得这样写

--- python/main.py
+++ python/main.py
@@ -3,5 +3,9 @@ from simnet.results import get_avg_by_itvars
 import pandas as pd
 
 if __name__ == '__main__':
-    sheet = read_csv("simulations", "test", "uniformTraffic")
-    print(get_runIDs(sheet))
+    sheet = read_csv("simulations", "exp", "fatTree")
+    vec_name = r'job-\d+-RoundFinishTime'
+    _df_veconly = sheet[sheet["name"].str.contains(pat=vec_name,
+                                                   na=False,
+                                                   regex=True)]
+    print(_df_veconly)
