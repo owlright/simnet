16353681da13fdb25b28b0f58700555d5515558a
jhsheng <jhsheng@mail.ustc.edu.cn>
2023-11-02 18:56:37 +0800

    feat: add plot of...

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import pstats
 pd.set_option('display.max_colwidth', 20)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 5000)
-print_avg_slowdown('fatTree')
+plot_loadbalance_slowdown('fatTree')
 # cProfile.run("print_avg_slowdown('fatTree')", 'restats')
 # p = pstats.Stats('restats')
 # p.sort_stats(pstats.SortKey.CUMULATIVE).print_stats(10)
