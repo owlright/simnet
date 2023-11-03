fa790926274f11d467b261f71e5045544208080f
jhsheng <jhsheng@mail.ustc.edu.cn>
2023-11-03 18:58:02 +0800

    feat(GlobalMetricCollector): 添加progreess info

--- src/simnet/mod/manager/GlobalMetricCollector.cc
+++ src/simnet/mod/manager/GlobalMetricCollector.cc
@@ -35,21 +35,60 @@ void GlobalMetricCollector::reportFlowStop(int jobid, int numWorkers, int worker
     if (roundMeter->counter == jobRoundMetric[jobid]->numWorkers) {
         emit(roundMeter->roundFctSignal, simTime() - roundMeter->startTime);
         emit(jobRCT, simTime() - roundMeter->startTime);
+        roundMeter->currentRound++;
         roundMeter->reset();
     }
 }
 
 void GlobalMetricCollector::reportFlowStop(int nodeId, simtime_t finishTime)
 {
+    flow_counter += 1;
     if (finishTime > allFlowsFinishedTime) {
         allFlowsFinishedTime = finishTime;
     }
+    // report progress in every 10 seconds
+    std::time_t now = std::time(nullptr);
+    if (std::difftime(now, last_time) > 10.0) {
+        scheduleAt(simTime(), stopWatch);
+        last_time = now;
+    }
 }
 
-void GlobalMetricCollector::finish()
+void GlobalMetricCollector::initialize(int stage)
 {
+    if (stage == INITSTAGE_LOCAL) {
+        globalView = findModuleFromTopLevel<GlobalView>("globalView", this);
+        if (globalView != nullptr) {
+            auto n = globalView->gethostIds().size();
+            auto globalConfig = getEnvir()->getConfigEx();
+            auto numFlows_cfg = globalConfig->getPerObjectConfigValue("**","numFlows");
+            auto numFlows = cStringTokenizer(numFlows_cfg).asIntVector().at(0);
+            totalFlowsNumber = n*numFlows;
+            last_time = std::time(nullptr);
+            stopWatch = new cMessage("stopWatch");
+        }
+        else
+            throw cRuntimeError("Fail to get globalView");
+    }
+}
+
+void GlobalMetricCollector::handleMessage(cMessage *msg)
+{
+    ASSERT(msg == stopWatch);
+    std::cout << CYAN << flow_counter << "/" << totalFlowsNumber <<" flows completed." << ENDC;
+    for (auto& [jobid, met]:jobRoundMetric) {
+        std::cout << CYAN << jobid << ": " << met->currentRound << ", ";
+    }
+    std::cout << ENDC;
+}
+
+void GlobalMetricCollector::finish() {
     // * help to set the sim-time-limit value so that progress percentile can be displayed correctly
-    std::cout << GREEN << "All flows stop at " << allFlowsFinishedTime << ENDC;
+    if (flow_counter != totalFlowsNumber) {
+        std::cout << RED << flow_counter << "/" << totalFlowsNumber <<" flows completed." << ENDC;
+    } else {
+        std::cout << GREEN <<  totalFlowsNumber << " flows stop at " << allFlowsFinishedTime << ENDC;
+    }
     std::cout << GREEN << "All jobs  stop at " << allJobsFinishedTime << ENDC;
 }
 
--- src/simnet/mod/manager/GlobalMetricCollector.h
+++ src/simnet/mod/manager/GlobalMetricCollector.h
@@ -1,17 +1,27 @@
 #pragma once
+#include <ctime>
 #include <omnetpp.h>
 #include <unordered_map>
 #include "simnet/common/Defs.h"
+#include "simnet/common/ModuleAccess.h"
+#include "simnet/mod/manager/GlobalView.h"
 using namespace omnetpp;
 
-class GlobalMetricCollector : public cModule
+class GlobalMetricCollector final: public cSimpleModule
 {
 public:
+    // for group coflows
     void reportFlowStart(int jobuid, int numWorkers, int workerId, simtime_t roundStartTime);
     void reportFlowStop(int jobuid, int numWorkers,  int workerId, simtime_t roundStopTime);
+    // for unicast flows
     void reportFlowStop(int nodeId, simtime_t finishTime);
-
+    ~GlobalMetricCollector() {
+        cancelAndDelete(stopWatch);
+    };
 protected:
+    virtual int numInitStages() const override { return NUM_INIT_STAGES; }
+    virtual void initialize(int stage) override;
+    virtual void handleMessage(cMessage* msg) override;
     void finish() override;
 
 private:
@@ -20,8 +30,14 @@ private:
     simtime_t allFlowsFinishedTime {0};
     simtime_t allJobsFinishedTime {0};
 
+    opp_component_ptr<GlobalView> globalView;
+    int64_t totalFlowsNumber{0};
+    int64_t flow_counter{0};
+    std::time_t last_time{0};
+
 private:
     struct JobRoundMetric {
+        int currentRound{0};
         int counter{0};
         int numWorkers;
         simtime_t startTime;
@@ -32,4 +48,5 @@ private:
         }
     };
     std::unordered_map<int, JobRoundMetric*> jobRoundMetric;
+    cMessage* stopWatch{nullptr};
 };
