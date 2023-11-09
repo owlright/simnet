8c6e697bcaa6dba975c9d3b0dd88e2ce32adb333
jhsheng <jhsheng@mail.ustc.edu.cn>
2023-11-09 19:23:07 +0800

    add boxplot

--- policy_flowslowdown_box.py
+++ policy_flowslowdown_box.py
@@ -1,7 +1,7 @@
 import bisect
 from analysis import *
 
-output_name = "policy_fsd.png"
+output_name = "policy_fsd_box.png"
 percentile_lowerbound = 0.99
 
 sheet = read_csv("simulations", "exp", "spineleaf", True)
@@ -22,11 +22,11 @@ _truncated_index = bisect.bisect_left(_pick_row, _truncated_duration)
 df = get_flows_slowdown(flows, runs, _truncated_index)
 df.sort_values(by=["load", "epsion"], inplace=True)
 
-policies = sorted(list(set([extract_str(x, 'aggPolicy') for x in runs.keys()])))
-epsions = sorted(list(set([extract_float(x, 'epsion') for x in runs.keys()])))
-loads = sorted(list(set([extract_float(x, 'load') for x in runs.keys()])))
+policies = sorted(list(set([extract_str(x, "aggPolicy") for x in runs.keys()])))
+epsions = sorted(list(set([extract_float(x, "epsion") for x in runs.keys()])))
+loads = sorted(list(set([extract_float(x, "load") for x in runs.keys()])))
 fig, ax = plt.subplots(1, len(loads), figsize=(50 / 2.54, 10 / 2.54))
-_pos = np.arange(1, 11) * (len(epsions) + 1)
+_pos = np.arange(1, 11) * (len(epsions) + 3)
 
 
 for col_index, load in enumerate(loads):
@@ -41,16 +41,24 @@ for col_index, load in enumerate(loads):
     _prefix0_flsz_x100 = [0] + flsz_x100
     bps = []
     for step, policy in enumerate(policies):
-        flsd: np.ndarray = _tmp[_tmp['policy'] == policy]["slowdown"].values[0]
+        flsd: np.ndarray = _tmp[_tmp["policy"] == policy]["slowdown"].values[0]
         flsd_intv = []
         for l, r in itertools.pairwise(_prefix0_flsz_x100):
             perlb = round(l + (r - l) * percentile_lowerbound)
-            flsd_intv.append(flsd[perlb:r].mean())
-        bp = ax[col_index].plot(_pos, flsd_intv, color=COLORS[step], marker=MARKERS[step])
+            flsd_intv.append(flsd[perlb:r])
+        bp = ax[col_index].boxplot(
+            flsd_intv,
+            False,
+            "",
+            widths=0.4,
+            patch_artist=True,
+            positions=_pos - step,
+            boxprops=dict(facecolor=COLORS[step]),
+        )
         bps.append(bp)
     # * xticks set only once each ax
     ax[col_index].set_xticks(_pos, [humanize(x) for x in flsz_x])
-    ax[col_index].legend([b[0] for b in bps], policies)
+    ax[col_index].legend([b["boxes"][0] for b in bps], policies)
     ax[col_index].set_xlabel(f"Flow size (Bytes) when load={load}")
 ax[0].set_ylabel("FCT slow down")
 
