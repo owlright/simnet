7d716699517b8b8f51ba67ad365d6f93249ed764
jhsheng <jhsheng@mail.ustc.edu.cn>
2023-11-10 14:17:25 +0800

    pd options

--- analysis/common.py
+++ analysis/common.py
@@ -3,3 +3,7 @@ import itertools
 import re
 import pandas as pd
 import matplotlib.pyplot as plt
+
+pd.set_option('display.max_colwidth', 20)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 5000)
