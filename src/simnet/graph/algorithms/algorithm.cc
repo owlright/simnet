0540e3f5a28586e729f1254ca4d60b9e003025d4
jhsheng <jhsheng@mail.ustc.edu.cn>
2023-11-20 23:55:33 +0800

    WIP(algorithm): add dijistra algorithm

--- src/simnet/graph/algorithms/algorithm.cc
+++ src/simnet/graph/algorithms/algorithm.cc
@@ -1,5 +1,13 @@
 #include "algorithm.h"
+#include <queue>
+#include <unordered_set>
+#include <utility>
 namespace simnet::algorithms {
+using std::make_pair;
+using std::pair;
+using std::priority_queue;
+using std::unordered_set;
+using diPair = pair<double, int>;
 void floyd_warshall(Mat<double>& distance)
 {
     int n = distance.size();
@@ -13,5 +21,54 @@ void floyd_warshall(Mat<double>& distance)
             }
         }
     }
+}
+// Comparison function to compare std::pair<double, int> based on the first element (double value)
+struct CompareDiPair {
+    bool operator()(const diPair& a, const diPair& b)
+    {
+        return a.first > b.first; // Comparing based on the first element (double value)
+    }
+};
+
+vector<int> dijistra(const Graph& g, int src, int dest)
+{
+    int n = g.get_vertices_number();
+    priority_queue<diPair, std::deque<diPair>, CompareDiPair> pq;
+    vector<double> dist(n, INFINITY);
+    vector<int> prev(n, -1);
+    pq.push(make_pair(0.0, src));
+    dist[src] = 0;
+    bool found = false;
+    unordered_set<int> visited { src };
+    while (!pq.empty()) {
+        auto u = pq.top().second;
+        pq.pop();
+        for (auto& [v, weight] : g.get_out_neighbors(u)) {
+            // If there is shorted path to v through u.
+            if (visited.find(v) == visited.end() && dist[v] > dist[u] + weight) {
+                // Updating distance of v
+                visited.insert(v);
+                dist[v] = dist[u] + weight;
+                prev[v] = u;
+                pq.push(make_pair(dist[v], v));
+            }
+            if (v == dest) {
+                found = true;
+                break;
+            }
+        }
+        if (found) {
+            break;
+        }
+    }
+    std::vector<int> path;
+    if (found) {
+        auto u = dest;
+        while (u != -1) {
+            path.insert(path.begin(), u);
+            u = prev[u];
+        }
+    }
+    return path;
 }
 }
--- src/simnet/graph/algorithms/algorithm.h
+++ src/simnet/graph/algorithms/algorithm.h
@@ -2,5 +2,9 @@
 #include "../graph.h"
 
 namespace simnet::algorithms {
-    void floyd_warshall(Mat<double>& distance);
+
+void floyd_warshall(Mat<double>& distance);
+vector<int> dijistra(const Graph& g, int src, int dest);
+Graph takashami_tree(const Graph& g, vector<int> sources, int root);
+
 }
