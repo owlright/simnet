647818d747ba84c75088c86e23a2a1da9f648c59
jhsheng <jhsheng@mail.ustc.edu.cn>
2023-11-21 18:33:06 +0800

    feat(Graph algorithm): extract only-branch-nodes tree

--- src/simnet/graph/algorithms/algorithm.h
+++ src/simnet/graph/algorithms/algorithm.h
@@ -6,5 +6,5 @@ namespace simnet::algorithms {
 void floyd_warshall(Mat<double>& distance);
 double dijistra(const Graph& g, int src, int dest, vector<int>* path = nullptr);
 Graph takashami_tree(const Graph& g, vector<int> sources, int root);
-
+Graph extract_branch_tree(const Graph& tree, const vector<int>&sources, int root, vector<int>* branch_nodes = nullptr);
 }
--- src/simnet/graph/algorithms/steiner-tree.cc
+++ src/simnet/graph/algorithms/steiner-tree.cc
@@ -23,4 +23,35 @@ Graph takashami_tree(const Graph& g, vector<int> sources, int root)
     tree.draw("tree");
     return tree;
 }
+
+Graph extract_branch_tree(const Graph& tree, const vector<int>& sources, int root, vector<int>* branch_nodes) {
+// ! make sure tree is actually a directed tree
+    Graph t;
+    unordered_set<int> visited;
+    for (auto& s:sources) {
+        auto node = s;
+        int edge_start = s;
+        while (node != root) {
+            if (visited.find(node) != visited.end()) {
+                t.add_edge(edge_start, node, tree.distance(edge_start, node));
+                break;
+            } else {
+                visited.insert(node);
+            }
+            if (tree.indegree(node) > 1) {
+                if (branch_nodes)
+                    branch_nodes->push_back(node);
+                t.add_edge(edge_start, node, tree.distance(edge_start, node));
+                edge_start = node;
+            }
+            node = tree.get_out_neighbors(node).at(0).first;
+        }
+        if (node == root) {
+            t.add_edge(edge_start, node, tree.distance(edge_start, node));
+        }
+    }
+    t.draw("extract_tree");
+    return t;
+}
+
 }
