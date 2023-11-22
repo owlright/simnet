f9ab495fda938cbad5cdd1e83ec4166cb488b06a
jhsheng <jhsheng@mail.ustc.edu.cn>
2023-11-22 18:18:24 +0800

    feat(algo/yenksp)

--- src/simnet/graph/algorithms/algorithm.cc
+++ src/simnet/graph/algorithms/algorithm.cc
@@ -8,6 +8,23 @@ using std::pair;
 using std::priority_queue;
 using std::unordered_set;
 using diPair = pair<double, int>;
+using CostPath = pair<double, Path>;
+
+bool operator==(const std::vector<int>& vec1, const std::vector<int>& vec2)
+{
+    if (vec1.size() != vec2.size()) {
+        return false; // Vectors have different sizes, they are not equal
+    }
+
+    for (size_t i = 0; i < vec1.size(); i++) {
+        if (vec1[i] != vec2[i]) {
+            return false; // Found a mismatching element, vectors are not equal
+        }
+    }
+
+    return true; // All elements are equal, vectors are equal
+}
+
 void floyd_warshall(Mat<double>& distance)
 {
     int n = distance.size();
@@ -80,6 +97,82 @@ double dijistra(const Graph& g, int src, int dest, vector<int>* path)
     return INFINITY; // * unreachable
 }
 
+vector<double> yenksp(Graph& g, int src, int dest, int K, vector<Path>& A)
+{
+    vector<double> distances;
+    Path p;
+    vector<CostPath> B;
+    distances.push_back(dijistra(g, src, dest, &p));
+    A.push_back(p);
+    for (int k = 1; k < K; k++) {
+        auto& prevPath = A.at(k - 1);
+        for (int i = 0; i < prevPath.size() - 1; i++) {
+            int spurNode = prevPath[i];
+            vector<int> rootPath(prevPath.begin(), prevPath.begin() + i + 1);
+            auto rootPathCost = dijistra(g, rootPath.front(), rootPath.back());
+            vector<double> weights_to_delete;
+            vector<Graph::Edge> edges_to_delete;
+            for (auto& p : A) {
+                vector<int> pslice(p.begin(), p.begin() + i + 1);
+                if (rootPath == pslice) {
+                    auto u = p.at(i);
+                    auto v = p.at(i + 1);
+                    if (g.has_edge(u,v)) {
+                        edges_to_delete.push_back({ u, v });
+                        weights_to_delete.push_back(g.weight(u, v));
+                        g.remove_edge(u, v);
+                    }
+                }
+            }
+            // * remove rootPathNode from Graph except spurNode
+            for (auto& rootPathNode : rootPath) {
+                if (rootPathNode == spurNode) {
+                    continue;
+                }
+                for (auto& [v, w] : g.out_neighbors(rootPathNode)) {
+                    edges_to_delete.push_back({ rootPathNode, v });
+                    weights_to_delete.push_back(w);
+                }
+                for (auto& [v, w] : g.in_neighbors(rootPathNode)) {
+                    edges_to_delete.push_back({ v, rootPathNode });
+                    weights_to_delete.push_back(w);
+                }
+                g.remove_node(rootPathNode);
+            }
+
+            vector<int> spurPath;
+            auto spurPathCost = dijistra(g, spurNode, dest, &spurPath);
+            if (spurPathCost != INFINITY) { // ! there may be no path from spurNode to dest
+                spurPathCost += rootPathCost;
+                ASSERT(spurPath.size() >= 2);
+                vector<int> totalPath(rootPath.begin(), rootPath.end());
+                totalPath.insert(totalPath.end(), spurPath.begin() + 1, spurPath.end());
+                bool found = false;
+                for (auto& [c, x] : B) {
+                    if (x == totalPath) {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    B.push_back({ spurPathCost, totalPath });
+                }
+                for (int index = 0; index < weights_to_delete.size(); index++) {
+                    g.add_edge(edges_to_delete[index].first, edges_to_delete[index].second, weights_to_delete[index]);
+                }
+            } else {
+                // do nothing
+                ASSERT(spurPath.empty());
+            }
+        }
+        if (B.empty())
+            break;
+        std::sort(B.begin(), B.end(), [](const auto& a, const auto& b) { return a.first < b.first; });
+        distances.push_back(B[0].first);
+        A.push_back(B[0].second);
+        B.erase(B.begin());
+    }
+    return distances;
 }
 
 }
--- src/simnet/graph/algorithms/algorithm.h
+++ src/simnet/graph/algorithms/algorithm.h
@@ -2,9 +2,10 @@
 #include "../graph.h"
 
 namespace simnet::algorithms {
-
+using Path = vector<int>;
 void floyd_warshall(Mat<double>& distance);
 double dijistra(const Graph& g, int src, int dest, vector<int>* path = nullptr);
 Graph takashami_tree(const Graph& g, vector<int> sources, int root);
-Graph extract_branch_tree(const Graph& tree, const vector<int>&sources, int root, vector<int>* branch_nodes = nullptr);
+Graph extract_branch_tree(const Graph& tree, const vector<int>& sources, int root, vector<int>* branch_nodes = nullptr);
+vector<double> yenksp(Graph& g, int src, int dest, int K, vector<Path>& A);
 }
