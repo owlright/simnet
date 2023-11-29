5f34bc991be05e17fa6ded9ba50e530548ba9f33
jhsheng <jhsheng@mail.ustc.edu.cn>
2023-11-30 01:33:15 +0800

    feat(python): group all slowdown together

--- epsion_fsd.py
+++ epsion_fsd.py
@@ -1,30 +1,23 @@
 import bisect
 from analysis import *
 
-output_name = "epsion_jct.png"
+output_name = "epsion_fsd.png"
 
 print("-" * 10, "reading data", "-" * 10)
 sheet = read_csv("simulations", "exp", "fatTreeK10LoadbalancePermutation", True)
-_kept_rows = ["jobRCT:vector"]
-jobs = get_vectors(
+_kept_rows = ["flowSize:vector", "fct:vector", "idealFct:vector", "jobRCT:vector"]
+flows = get_vectors(
     sheet,
     names=_kept_rows,
     module="FatTree",
 )
 runs = get_runIDs(sheet, by="iterationvars")
 assert isinstance(runs, dict)
-# * a little hack to transpose the columns
-jobs = jobs.set_index(["runID", "name"], drop=True)["vecvalue"].unstack()  # pivot the name column
-df = pd.DataFrame(columns=["load", "policy", "epsion", "jct"])
+truncate_vectime(flows, runs)
 
-for itervar, repetition_ids in runs.items():
-    policy, load, epsion = extract_iterationvar(itervar)
-    print(load, policy, epsion)
-    jobreps = jobs.loc[repetition_ids]
-    jct = np.concatenate(jobreps["jobRCT:vector"].values)
-    df.loc[len(df.index)] = [load, policy, epsion, jct]   # pyright: ignore reportGeneralTypeIssues
+print("-" * 10, "calc slowdown", "-" * 10)
+df = get_flows_slowdown(flows, runs)
 
-jobs = df
 epsions = sorted(list(set([extract_float(x, "epsion") for x in runs.keys()])))
 loads = sorted(list(set([extract_float(x, "load") for x in runs.keys()])))
 fig, ax = plt.subplots()
@@ -33,20 +26,20 @@ _bar_width = 0.2
 bps = []
 
 for step, epsion in enumerate(epsions):
-    current_load = jobs[jobs["epsion"] == epsion]
-    jct_mean = []
+    current_load = df[df["epsion"] == epsion]
+    fsd_mean = []
     for load in loads:
         print(epsion, load)
         current_epsion = current_load[current_load["load"] == load]
-        jct_mean.append(current_epsion['jct'].values[0].mean())
+        fsd_mean.append(current_epsion['slowdown'].values[0].mean())
 
-    bp = ax.bar(_pos + step*_bar_width, jct_mean, _bar_width, hatch=HATCHES[step])
+    bp = ax.bar(_pos + step*_bar_width, fsd_mean, _bar_width, hatch=HATCHES[step])
     bps.append(bp)
 
 ax.set_xticks(_pos, loads)
 ax.legend([b[0] for b in bps], epsions)
 ax.set_xlabel(f"load")
-ax.set_ylabel("job mean round completion time")
+ax.set_ylabel("mean flow slowdown")
 fig.subplots_adjust(left=0.05, bottom=0.15, right=0.95, top=0.95)
 fig.subplots_adjust(wspace=0.1)
 fig.savefig(output_name)
