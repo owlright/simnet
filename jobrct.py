d9ab90c4212a1cdac366bd8f6bc1ee0f71453cf2
jhsheng <jhsheng@mail.ustc.edu.cn>
2023-12-17 22:24:37 +0800

    adjust style

--- jobrct.py
+++ jobrct.py
@@ -1,6 +1,18 @@
 from analysis import *
 import argparse
+from matplotlib import rcParams
 
+rcParams.update(
+    {
+        "font.family": "serif",
+        "mathtext.fontset": "stix",
+        "font.serif": ["SimSun"],
+        "axes.unicode_minus": False,
+    }
+)
+ticklabel_style = {
+    "fontname": "Times New Roman",
+}
 parser = argparse.ArgumentParser(description="draw flow slowdown in each interval under various load")
 parser.add_argument(
     "-c",
@@ -24,7 +36,7 @@ parser.add_argument(
 )
 
 args = parser.parse_args()
-output_name = "jobrct_" + args.legendname + ".png"
+output_name = "jobrct_" + args.legendname + ".pdf"
 
 
 print("-" * 10, "reading data", "-" * 10)
@@ -53,7 +65,8 @@ jobs = df
 policies = sorted(list(set([extract_str(x, "aggPolicy") for x in runs.keys()])))
 epsions = sorted(list(set([extract_float(x, "epsion") for x in runs.keys()])))
 loads = sorted(list(set([extract_float(x, "load") for x in runs.keys()])))
-fig, ax = plt.subplots()
+
+fig, ax = plt.subplots(figsize=(12 / 2.54, 7 / 2.54))
 _pos = np.arange(len(loads))
 _bar_width = 0.2
 bps = []
@@ -67,20 +80,35 @@ for step, legend in enumerate(legends):
     current_load = jobs[jobs[args.legendname] == legend]
     jct_mean = []
     for load in loads:
-        print(legend, load)
         current_epsion = current_load[current_load["load"] == load]
         jct_mean.append(current_epsion["jct"].values[0].mean())
+        print(legend, load, current_epsion["jct"].values[0].mean())
 
     bp = ax.bar(_pos + step * _bar_width, jct_mean, _bar_width, hatch=HATCHES[step], color=COLORS[step], ec="black")
     bps.append(bp)
 
-ax.tick_params(top=True, right=True, direction="in")
 
+plt.xticks(fontname="Times New Roman")
+
+ax.tick_params(direction="in")
 ax.set_xticks(_pos + _bar_width / 2, loads)
-ax.legend([b[0] for b in bps], [f"{args.legendname}={legend}" for legend in legends], frameon=True)
-ax.set_xlabel(f"Load")
-ax.set_ylabel("job mean round completion time")
-# fig.subplots_adjust(left=0.05, bottom=0.15, right=0.95, top=0.95)
+if args.legendname == "epsion":
+    ax.legend([b[0] for b in bps], [f"K={0 if legend == 0.0 else 10}" for legend in legends], frameon=False, prop={'family': "Times New Roman"}, ncol=2, columnspacing=0.8,loc='upper right',bbox_to_anchor=(1, 1.04))
+    ax.set_ylim(ymin=0, ymax=0.085)
+elif args.legendname == "policy":
+    ax.legend([b[0] for b in bps], ["ATP", "ATSR"], frameon=False, prop={'family': "Times New Roman"}, ncol=2,columnspacing=0.8,loc='upper right',bbox_to_anchor=(1, 1.04))
+    ax.set_ylim(ymin=0, ymax=0.085)
+
+ax.set_xlabel("负载")
+ax.set_ylabel("汇聚组平均每轮完成时间/s")
+ax.set_axisbelow(True)
+ax.yaxis.grid(color='gray', linestyle='dashed', alpha=0.5)
+ax.xaxis.grid(color='gray', linestyle='dashed', alpha=0.5)
+
+fig.subplots_adjust(left=0.12, bottom=0.15, right=0.99, top=0.99)
 # fig.subplots_adjust(wspace=0.1)
-fig.savefig(output_name)
+fig.savefig(output_name, dpi=600)
 print(f"output {output_name}")
+import os
+paper_path = os.path.join(os.getcwd(), f"../Documents/dissertation/figures/chapter4/{output_name}")
+fig.savefig(paper_path, dpi=600)
