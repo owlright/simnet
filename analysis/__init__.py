468d2d4a7159f9b34bbb559240b93314b6b54e20
jhsheng <jhsheng@mail.ustc.edu.cn>
2024-03-07 20:16:44 +0800

    fix color codes

--- analysis/__init__.py
+++ analysis/__init__.py
@@ -1,6 +1,7 @@
 from .util import *
 from .results import *
 from .common import *
-COLORS = ["#8ecfc9", "#ffbe7a", "FA7F6F", "82B0D2"]
+
+COLORS = ["#8ecfc9", "#ffbe7a", "#fa7f6f", "#82b0d2", "#BEB8DC", "#E7DAD2"]
 MARKERS = [".", "s", "*", "+"]
-HATCHES = ['//', r'\\', '..','x']
+HATCHES = ["//", r"\\", "..", "x", "o", "+"]
